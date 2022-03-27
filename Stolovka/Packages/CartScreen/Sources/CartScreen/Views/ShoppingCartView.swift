a035470f11c3f540b5ac10d9379d2db7eff83fa7
Nikita <Tomskrenta@mail.ru>
2022-03-28 05:58:58 +1300

    Removed stack view from ShoppingCartView

--- Stolovka/Packages/CartScreen/Sources/CartScreen/Views/ShoppingCartView.swift
+++ Stolovka/Packages/CartScreen/Sources/CartScreen/Views/ShoppingCartView.swift
@@ -10,26 +10,6 @@ class ShoppingCartView: UIView {
 
     let shoppingCartTableView = ShoppingCartTableView()
 
-    private lazy var scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.addSubview(scrollViewContentView)
-        return scrollView
-    }()
-
-    private lazy var scrollViewContentView: UIView = {
-        let view = UIView()
-        view.addSubview(stackView)
-        return view
-    }()
-
-    private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [shoppingCartTableView])
-        stackView.axis = .vertical
-        stackView.spacing = 16
-        stackView.backgroundColor = SharedResources.Asset.Colors.tableViewBackground.color
-        return stackView
-    }()
-
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -41,22 +21,9 @@ class ShoppingCartView: UIView {
 
     private func setupView() {
         backgroundColor = SharedResources.Asset.Colors.tableViewBackground.color
-        addSubview(scrollView)
-
-        scrollView.snp.makeConstraints { make in
-            make.edges.equalTo(safeAreaLayoutGuide)
-        }
-
-        scrollViewContentView.snp.makeConstraints { make in
+        addSubview(shoppingCartTableView)
+        shoppingCartTableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
-            make.width.equalToSuperview()
-            make.height.equalToSuperview().priority(.low)
         }
-
-        stackView.snp.makeConstraints { make in
-            make.leading.trailing.top.equalToSuperview()
-            make.bottom.equalToSuperview()
-        }
-
     }
 }
