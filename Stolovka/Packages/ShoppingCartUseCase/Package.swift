d204ca9079133b534359ebb707f76b5ff55b018a
Nikita <Tomskrenta@mail.ru>
2022-03-28 20:01:50 +1300

    ShoppingCartUseCasePackage
    ShoppingCartUseCasePackage

--- Stolovka/Packages/ShoppingCartUseCase/Package.swift
+++ Stolovka/Packages/ShoppingCartUseCase/Package.swift
@@ -1,37 +1,26 @@
-// swift-tools-version:5.5.0
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "CartScreen",
+    name: "ShoppingCartUseCase",
     platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CartScreen",
-            targets: ["CartScreen"]
-        )
+            name: "ShoppingCartUseCase",
+            targets: ["ShoppingCartUseCase"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "../SharedResources"),
-        .package(path: "../Managers"),
-        .package(path: "../Models"),
-        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CartScreen",
-            dependencies: [
-                "SharedResources",
-                "Managers",
-                "Models",
-                "SnapKit"
-            ]
-        )
+            name: "ShoppingCartUseCase",
+            dependencies: []),
     ]
 )
