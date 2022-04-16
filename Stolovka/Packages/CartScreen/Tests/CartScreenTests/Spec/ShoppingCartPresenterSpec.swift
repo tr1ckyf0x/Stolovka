7e0ead672423466ce6d9e97cb524564999f5e793
Vladislav Lisyanskiy <vladislav.lisyanskiy@gmail.com>
2022-04-16 15:51:48 +0400

    Removed extra spaces

--- Stolovka/Packages/CartScreen/Tests/CartScreenTests/Spec/ShoppingCartPresenterSpec.swift
+++ Stolovka/Packages/CartScreen/Tests/CartScreenTests/Spec/ShoppingCartPresenterSpec.swift
@@ -1,6 +1,6 @@
 //
 //  ShoppingCartPresenterSpec.swift
-//  
+//
 //
 //  Created by Vladislav Lisianskii on 10.04.2022.
 //
@@ -16,40 +16,40 @@ import Models
 // swiftlint:disable closure_body_length
 
 final class StolovkaMainScreenPresenterSpec: QuickSpec {
-    
+
     override func spec() {
-        
+
         var sut: ShoppingCartPresenter!
-        
+
         var shoppingCartControllerInputMock: ShoppingCartControllerInputMock!
         var shoppingCartTableViewManagerMock: ShoppingCartTableManagerProtocolMock!
         var fetchCartItemsUseCaseMock: AsyncUseCaseMock<Void, [CountableContainer<FoodItem>]>!
         var addToCartUseCaseMock: AsyncUseCaseMock<FoodItem, Void>!
         var removeFromCartUseCaseMock: AsyncUseCaseMock<FoodItem, Void>!
-        
+
         beforeEach {
             sut = ShoppingCartPresenter()
-            
+
             shoppingCartControllerInputMock = ShoppingCartControllerInputMock()
             shoppingCartTableViewManagerMock = ShoppingCartTableManagerProtocolMock()
             fetchCartItemsUseCaseMock = AsyncUseCaseMock()
             addToCartUseCaseMock = AsyncUseCaseMock()
             removeFromCartUseCaseMock = AsyncUseCaseMock()
-            
+
             sut.viewController = shoppingCartControllerInputMock
             sut.shoppingCartTableViewManager = shoppingCartTableViewManagerMock
             sut.fetchCartItemsUseCase = fetchCartItemsUseCaseMock
             sut.addToCartUseCase = addToCartUseCaseMock
             sut.removeFromCartUseCase = removeFromCartUseCaseMock
         }
-        
+
         describe("viewDidLoad(_:)") {
             it("should call fetchCartItemsUseCase.executeAsync()") {
                 sut.viewDidLoad(shoppingCartControllerInputMock)
-                
+
                 expect(fetchCartItemsUseCaseMock.didCallExecuteAsync).to(beTrue())
             }
-            
+
             context("fetchCartItemsUseCase result is success") {
                 beforeEach {
                     let foodItem = FoodItem(
@@ -60,25 +60,25 @@ final class StolovkaMainScreenPresenterSpec: QuickSpec {
                         isLikedByUser: false,
                         itemID: ""
                     )
-                    
+
                     let result = [CountableContainer<FoodItem>(item: foodItem, quantity: 1)]
                     fetchCartItemsUseCaseMock.result = .success(result)
                 }
-                
+
                 it("should call recommendationsCollectionManager.setFoodItems(_:)") {
                     sut.viewDidLoad(shoppingCartControllerInputMock)
-                    
+
                     expect(shoppingCartTableViewManagerMock.setShoppingCartFoodItemsCalled).to(beTrue())
                 }
-                
+
                 it("should call viewController.reloadShoppingCartTableView()") {
                     sut.viewDidLoad(shoppingCartControllerInputMock)
-                    
+
                     expect(shoppingCartControllerInputMock.reloadShoppingCartTableViewCalled).to(beTrue())
                 }
             }
         }
-        
+
         describe("view(_:didTapAddButtonFor:)") {
             let foodItem = FoodItem(
                 name: "",
@@ -88,48 +88,48 @@ final class StolovkaMainScreenPresenterSpec: QuickSpec {
                 isLikedByUser: false,
                 itemID: ""
             )
-            
+
             let foodItemContainer = CountableContainer(item: foodItem, quantity: 1)
-            
+
             it("Should call addToCartUseCase.executeAsync(_:)") {
                 sut.view(shoppingCartControllerInputMock, didTapAddButtonFor: foodItemContainer)
-                
+
                 expect(addToCartUseCaseMock.didCallExecuteAsync).to(beTrue())
             }
-            
+
             context("addToCartUseCase.executeAsync(_:) returns success") {
-                
+
                 beforeEach {
                     addToCartUseCaseMock.result = .success(Void())
                 }
-                
+
                 it("should call fetchCartItemsUseCase.executeAsync()") {
                     sut.view(shoppingCartControllerInputMock, didTapAddButtonFor: foodItemContainer)
-                    
+
                     expect(fetchCartItemsUseCaseMock.didCallExecuteAsync).to(beTrue())
                 }
-                
+
                 context("fetchCartItemsUseCase?.executeAsync() returns success") {
-                    
+
                     beforeEach {
                         fetchCartItemsUseCaseMock.result = .success([])
                     }
-                    
+
                     it("should call shoppingCartTableViewManager.setShoppingCartFoodItems(_:)") {
                         sut.view(shoppingCartControllerInputMock, didTapAddButtonFor: foodItemContainer)
-                        
+
                         expect(shoppingCartTableViewManagerMock.setShoppingCartFoodItemsCalled).to(beTrue())
                     }
-                    
+
                     it("should call viewController.reloadShoppingCartTableView()") {
                         sut.view(shoppingCartControllerInputMock, didTapAddButtonFor: foodItemContainer)
-                        
+
                         expect(shoppingCartControllerInputMock.reloadShoppingCartTableViewCalled).to(beTrue())
                     }
                 }
             }
         }
-        
+
         describe("view(_:didTapRemoveButtomFor:)") {
             let foodItem = FoodItem(
                 name: "",
@@ -139,35 +139,35 @@ final class StolovkaMainScreenPresenterSpec: QuickSpec {
                 isLikedByUser: false,
                 itemID: ""
             )
-            
+
             let foodItemContainer = CountableContainer(item: foodItem, quantity: 1)
-            
+
             it("should call removeFromCartUseCase.executeAsync(_:)") {
                 sut.view(shoppingCartControllerInputMock, didTapRemoveButtonFor: foodItemContainer)
-                
+
                 expect(removeFromCartUseCaseMock.didCallExecuteAsync).to(beTrue())
             }
-            
+
             context("removeFromCartUseCase?.executeAsync() returns success") {
-                
+
                 beforeEach {
                     removeFromCartUseCaseMock.result = .success(Void())
                 }
-                
+
                 it("should call fetchCartItemsUseCase.executeAsync()") {
                     sut.view(shoppingCartControllerInputMock, didTapRemoveButtonFor: foodItemContainer)
-                    
+
                     expect(fetchCartItemsUseCaseMock.didCallExecuteAsync).to(beTrue())
                 }
                 context("fetchCartItemsUseCase.executeAsync() returns true") {
-                    
+
                     beforeEach {
                         fetchCartItemsUseCaseMock.result = .success([])
                     }
-                    
+
                     it("should call viewController.reloadShoppingCartTableView()") {
                         sut.view(shoppingCartControllerInputMock, didTapRemoveButtonFor: foodItemContainer)
-                        
+
                         expect(shoppingCartControllerInputMock.reloadShoppingCartTableViewCalled).to(beTrue())
                     }
                 }
@@ -177,3 +177,4 @@ final class StolovkaMainScreenPresenterSpec: QuickSpec {
 }
 // swiftlint:enable function_body_length
 // swiftlint:enable closure_body_length
+
