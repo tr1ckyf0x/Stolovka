2c9bb41dce2f3db81bd99f3251303cde1cb290d8
Nikita <Tomskrenta@mail.ru>
2022-05-05 05:25:12 +1200

    Stepper

--- Stolovka/Packages/CartScreen/Sources/CartScreen/Views/ShoppingCartTableViewCell.swift
+++ Stolovka/Packages/CartScreen/Sources/CartScreen/Views/ShoppingCartTableViewCell.swift
@@ -7,6 +7,7 @@
 import Foundation
 import SharedResources
 import UIKit
+import UIComponents
 import SnapKit
 import Models
 
@@ -26,6 +27,8 @@ final class ShoppingCartTableViewCell: UITableViewCell {
     private var shoppingCartFoodItem: CountableContainer<FoodItem>?
     weak var delegate: ShoppingCartViewCellDelegate?
 
+    private var stepper = Stepper()
+    
     private lazy var itemImageView: UIImageView = {
         let image = UIImageView()
         image.contentMode = .scaleAspectFit
@@ -61,38 +64,6 @@ final class ShoppingCartTableViewCell: UITableViewCell {
         return label
     }()
 
-    private lazy var quantityLabel: UILabel = {
-        let label = UILabel()
-        label.layer.masksToBounds = true
-        label.layer.cornerRadius = 8
-        label.layer.borderColor = SharedResources.Asset.Colors.primaryText.color.cgColor
-        label.layer.borderWidth = 1
-        label.textAlignment = .center
-        label.textColor = SharedResources.Asset.Colors.primaryText.color
-        label.font = .systemFont(ofSize: 22)
-        label.minimumScaleFactor = 0.6
-        label.backgroundColor = SharedResources.Asset.Colors.tableViewBackground.color
-        return label
-    }()
-
-    private lazy var addOneItemButton: UIButton = {
-        let button = UIButton(type: .system)
-        let image = UIImage(sfSymbol: SFSymbol.plusCircle)
-        button.setImage(image, for: .normal)
-        button.tintColor = .systemBlue
-        button.addTarget(self, action: #selector(pressedAddOneItem), for: .touchUpInside)
-        return button
-    }()
-
-    private lazy var removeOneItemButton: UIButton = {
-        let button = UIButton(type: .system)
-        let image = UIImage(sfSymbol: SFSymbol.minusCircle)
-        button.setImage(image, for: .normal)
-        button.tintColor = .systemBlue
-        button.addTarget(self, action: #selector(pressedRemoveOneItem), for: .touchUpInside)
-        return button
-    }()
-
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupView()
@@ -100,12 +71,6 @@ final class ShoppingCartTableViewCell: UITableViewCell {
 
     required init?(coder: NSCoder) {
         fatalError("Not Implemented")
-    }
-
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        guard #available(iOS 13.0, *) else { return }
-        guard traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) else { return }
-        quantityLabel.layer.borderColor = SharedResources.Asset.Colors.primaryText.color.cgColor
     }
 }
 
@@ -129,12 +94,9 @@ extension ShoppingCartTableViewCell {
     private func setupView() {
         self.backgroundColor = Asset.Colors.tableViewBackground.color
         [
-            quantityLabel,
             itemImageView,
             itemDescriptionLabel,
             itemNameLabel,
-            addOneItemButton,
-            removeOneItemButton,
             itemPriceLabel
         ].forEach(contentView.addSubview(_:))
 
@@ -151,13 +113,6 @@ extension ShoppingCartTableViewCell {
             make.right.equalTo(quantityLabel.snp.right)
         }
 
-        quantityLabel.snp.makeConstraints { make in
-            make.right.equalToSuperview().inset(8)
-            make.centerY.equalToSuperview()
-            make.height.equalToSuperview().multipliedBy(0.33)
-            make.width.equalTo(quantityLabel.snp.height)
-        }
-
         itemDescriptionLabel.snp.makeConstraints { make in
             make.left.equalTo(itemNameLabel.snp.left)
             make.top.equalTo(itemNameLabel.snp.bottom).offset(8)
@@ -172,18 +127,6 @@ extension ShoppingCartTableViewCell {
             make.right.equalTo(quantityLabel.snp.right)
         }
 
-        addOneItemButton.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().inset(8)
-            make.height.equalToSuperview().multipliedBy(0.13)
-            make.width.equalTo(addOneItemButton.snp.height)
-            make.right.equalTo(quantityLabel.snp.right)
-        }
-
-        removeOneItemButton.snp.makeConstraints { make in
-            make.bottom.equalTo(addOneItemButton.snp.bottom)
-            make.height.width.equalTo(addOneItemButton.snp.height)
-            make.left.equalTo(quantityLabel.snp.left)
-        }
     }
 
     @objc
--- Stolovka/Packages/UIComponents/Sources/UIComponents/Stepper/Stepper.swift
+++ Stolovka/Packages/UIComponents/Sources/UIComponents/Stepper/Stepper.swift
@@ -12,20 +12,24 @@ import SnapKit
 final class Stepper: UIView {
 
     private lazy var incrementButton: UIButton = { button in
-        button
+        button.backgroundColor = Asset.Colors.stepperOperator.color
+        return button
     }(UIButton())
 
     private lazy var decrementButton: UIButton = { button in
-        button
+        button.backgroundColor = Asset.Colors.stepperOperator.color
+        return button
     }(UIButton())
 
     private lazy var countLabelBackgroundView: UIView = { view in
         view.layer.cornerRadius = 4
+        view.backgroundColor = Asset.Colors.tableViewBackground.color
         return view
     }(UIView())
 
     private lazy var countLabel: UILabel = { label in
-        label
+        label.textColor = Asset.Colors.stepperNumber.color
+        return label
     }(UILabel())
 
     override init(frame: CGRect) {
@@ -42,5 +46,41 @@ final class Stepper: UIView {
 // MARK: - Private methods
 extension Stepper {
     private func setupView() {
+        self.backgroundColor = Asset.Colors.stepperBackground.color
+        self.layer.cornerRadius = 4
+
+        [
+            incrementButton,
+            countLabelBackgroundView,
+            decrementButton
+        ].forEach(self.addSubview(_:))
+
+        countLabelBackgroundView.snp.makeConstraints { make in
+            make.height.equalToSuperview()
+            make.width.equalTo(countLabelBackgroundView.snp.height)
+            make.centerX.centerY.equalToSuperview()
+        }
+
+        incrementButton.snp.makeConstraints { make in
+            make.height.equalToSuperview()
+            make.width.equalTo(incrementButton.snp.height)
+            make.left.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.right.equalTo(countLabelBackgroundView.snp.left)
+        }
+
+        decrementButton.snp.makeConstraints { make in
+            make.height.equalToSuperview()
+            make.width.equalTo(decrementButton.snp.height)
+            make.right.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.left.equalTo(countLabelBackgroundView.snp.left)
+        }
+
+        countLabelBackgroundView.addSubview(countLabel)
+
+        countLabel.snp.makeConstraints { make in
+            make.centerY.centerX.height.width.equalToSuperview()
+        }
     }
 }
