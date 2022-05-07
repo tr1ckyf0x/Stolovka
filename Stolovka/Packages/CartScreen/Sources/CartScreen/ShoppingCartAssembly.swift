2b95be0b9496e6c95442b0fadc9b7710f15fa03f
NikitaShvadlenko <75701351+NikitaShvadlenko@users.noreply.github.com>
2022-05-08 06:50:37 +1200

    Update Stolovka/Packages/CartScreen/Sources/CartScreen/ShoppingCartAssembly.swift
    Co-authored-by: Vladislav Lisyanskiy <vladislav.lisyanskiy@gmail.com>

--- Stolovka/Packages/CartScreen/Sources/CartScreen/ShoppingCartAssembly.swift
+++ Stolovka/Packages/CartScreen/Sources/CartScreen/ShoppingCartAssembly.swift
@@ -17,7 +17,6 @@ public enum ShoppingCartAssembly {
         let removeFromCartUseCase = RemoveShoppingCartItemUseCase(shoppingCart: shoppingCartManager)
 
         shoppingCartTableViewManager.delegate = viewController
-
         viewController.setShoppingCartDataSource(dataSource: shoppingCartTableViewManager)
         presenter.shoppingCartTableViewManager = shoppingCartTableViewManager
         presenter.fetchCartItemsUseCase = fetchShoppingCartUseCase
